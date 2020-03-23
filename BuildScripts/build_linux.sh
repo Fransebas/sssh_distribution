7ddbe9ed5869bb4d7c69df893fb2efcb5b0b6506
fransebas <ffransebas@gmail.com>
2020-03-22 23:16:46 -0600

    building script

--- BuildScripts/build_linux.sh
+++ BuildScripts/build_linux.sh
@@ -3,4 +3,12 @@ export GOOS="linux"
 export GOARCH="arm"
 export CGO_ENABLED=1
 
-go build -o builds/ubuntu/sssh_server
+
+# $1 is the version
+
+mkdir builds/ubuntu$1
+
+cp "builds/ubuntu/install.sh" builds/ubuntu$1/install.sh
+cp "builds/ubuntu/ssshserver.sh" builds/ubuntu$1/ssshserver.sh
+
+go build -o "builds/ubuntu$1/sssh_server"
--- BuildScripts/build_macos.sh
+++ BuildScripts/build_macos.sh
@@ -2,6 +2,6 @@
 
 # $1 is the version
 
-mkdir macos$1
+mkdir builds/macos$1
 
 go build -o "builds/macos$1/sssh_server"
