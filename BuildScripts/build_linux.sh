eeabcd6dbb8082f36930ff43ce7d54313a40ad5e
fransebas <ffransebas@gmail.com>
2020-03-22 23:18:51 -0600

    typo

--- BuildScripts/build_linux.sh
+++ BuildScripts/build_linux.sh
@@ -9,6 +9,6 @@ export CGO_ENABLED=1
 mkdir builds/ubuntu$1
 
 cp "builds/ubuntu/install.sh" builds/ubuntu$1/install.sh
-cp "builds/ubuntu/ssshserver.sh" builds/ubuntu$1/ssshserver.sh
+cp "builds/ubuntu/ssshserver" builds/ubuntu$1/ssshserver
 
 go build -o "builds/ubuntu$1/sssh_server"
