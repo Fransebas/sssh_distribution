d0daa9b0708c528c317af9d90ff84d23d1b397c0
fransebas <ffransebas@gmail.com>
2020-03-23 23:10:04 -0600

    Improving ubuntu installation

--- BuildScripts/build_linux.sh
+++ BuildScripts/build_linux.sh
@@ -3,8 +3,6 @@
 # $1 is the version
 mkdir "builds/ubuntu$1"
 
-cp "builds/ubuntu/install.sh" "builds/ubuntu$1/install.sh"
-cp "builds/ubuntu/ssshserver" "builds/ubuntu$1/ssshserver"
-cp "builds/ubuntu/sssh_server.service" "builds/ubuntu$1/sssh_server.service"
+cp -R "builds/ubuntu" "builds/ubuntu$1"
 
 go build -o "builds/ubuntu$1/sssh_server"
