af494f7fc095d783a187aa2f8aa5728ef0021042
fransebas <fransebas@gmail.com>
2020-03-23 20:12:04 -0600

    even more improvements

--- BuildScripts/build_linux.sh
+++ BuildScripts/build_linux.sh
@@ -5,5 +5,6 @@ mkdir "builds/ubuntu$1"
 
 cp "builds/ubuntu/install.sh" "builds/ubuntu$1/install.sh"
 cp "builds/ubuntu/ssshserver" "builds/ubuntu$1/ssshserver"
+cp "builds/ubuntu/sssh_server.service" "builds/ubuntu$1/sssh_server.service"
 
 go build -o "builds/ubuntu$1/sssh_server"
---
+++ builds/ubuntu0.0.8/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# needs to be root user
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+
+
+cp ./sssh_server /usr/local/bin/sssh_server
+cp ./ssshserver /etc/init.d/ssshserver
+cp ./sssh_server.service /lib/systemd/system/sssh_server.service
+
+echo "Installation finished"
