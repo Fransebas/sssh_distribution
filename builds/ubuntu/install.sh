68481e362e024db77f238b303f7aaf914987c609
fransebas <ffransebas@gmail.com>
2020-03-23 23:05:52 -0600

    Forgot to copy the config file to etc

--- builds/ubuntu/install.sh
+++ builds/ubuntu/install.sh
@@ -8,6 +8,7 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 cp ./sssh_server /usr/local/bin/sssh_server
 cp ./ssshserver /etc/init.d/ssshserver
 cp ./sssh_server.service /lib/systemd/system/sssh_server.service
+cp ./sssh.conf /etc/sssh.conf
 
 mkdir /etc/sssh
 
