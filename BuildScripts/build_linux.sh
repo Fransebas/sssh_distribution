2b9d2a40b5ab63bdce0dda6c3cdc3ae1f15f3557
fransebas <ffransebas@gmail.com>
2020-04-08 20:48:54 -0500

    fixing installation

--- BuildScripts/build_linux.sh
+++ BuildScripts/build_linux.sh
@@ -21,6 +21,8 @@ go build -o ${FOLDER}/usr/local/bin/sssh_server
 cp builds/ubuntu/sssh_server.service ${FOLDER}/lib/systemd/system/sssh_server.service
 cp builds/ubuntu/sssh.conf ${FOLDER}/etc/sssh.conf
 
+cp -r builds/ubuntu/DEBIAN ${FOLDER}/DEBIAN
+
 echo "Version: ${VERSION}" >> ${FOLDER}/DEBIAN/control
 
 mkdir -p ${OUTPUT}
